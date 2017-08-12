8029c3f3f3936ae176a2e362da7b29c3ff10a9a6
Hien-HzN <Hien.3st@gmail.com>
2017-08-12 09:20:56 +0700

    commit number ulti

---
+++ Shop.Of.Bong/Ulti/NumberUlti.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Of.Bong.Ulti
+{
+    public class NumberUlti
+    {
+        public bool IsNumber(string value)
+        {
+            int result;
+            return int.TryParse(value, out result);
+        }
+
+        public int ConvertStringToInt(string value)
+        {
+            if (IsNumber(value))
+            {
+                return Convert.ToInt32(value);
+            }
+            return 0;
+        }
+    }
+}
