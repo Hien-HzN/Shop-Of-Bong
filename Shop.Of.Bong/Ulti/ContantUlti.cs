caac9d551a8f071b33c580aa7aca6ac191133ef3
Hien-HzN <Hien.3st@gmail.com>
2017-08-12 09:07:36 +0700

    Commit common function

---
+++ Shop.Of.Bong/Ulti/ContantUlti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Of.Bong.Ulti
+{
+    public static class ContantUlti
+    {
+        public static String DATETIME_FORMAT_YYYYMMDD = "yyyyMMdd";
+        public static String DATETIME_DEFAULT = "1970/01/01";
+
+    }
+}
---
+++ Shop.Of.Bong/Ulti/DatetimeUlti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Of.Bong.Ulti
+{
+    public class DatetimeUlti
+    {
+        public DateTime ConvertStringToDate(string value)
+        {
+            DateTime date;
+
+            return date;
+        }
+    }
+}
